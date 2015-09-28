575d2d0364f72a6a916a613bd21e9a26ddaf01eb
supzann3 <susan.hehe01@gmail.com>
2015-09-28 17:49:51 -0400

    add figaro.rb file

---
+++ config/initializers/figaro.rb
@@ -0,0 +1,6 @@
+Figaro.require_keys("google_places", "Consumer_Key", "Consumer_Secret","Token","Token_Secret")
+google_places=ENV["google_places"]
+Consumer_Key=ENV["Consumer_Key"]
+Consumer_Secret=ENV["Consumer_Secret"]
+Token=ENV["Token"]
+Token_Secret=ENV["Token_Secret"]
