6e88185a38c6c96032a5cbea46050ab4ea4628ff
supzann3 <susan.hehe01@gmail.com>
2015-09-29 13:59:49 -0400

    parse json from yelp and add column alias to tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+tag_names={"Active Life"=>"active",
+ "Arts & Entertainment"=>"arts",
+ "Automotive"=>"auto",
+ "Beauty & Spas"=>"beautysvc",
+ "Bicycles"=>"bicycles",
+ "Education"=>"education",
+ "Event Planning & Services"=>"eventservices",
+ "Financial Services"=>"financialservices",
+ "Food"=>"food",
+ "Health & Medical"=>"health",
+ "Home Services"=>"homeservices",
+ "Hotels & Travel"=>"hotelstravel",
+ "Local Flavor"=>"localflavor",
+ "Local Services"=>"localservices",
+ "Mass Media"=>"massmedia",
+ "Nightlife"=>"nightlife",
+ "Pets"=>"pets",
+ "Professional Services"=>"professional",
+ "Public Services & Government"=>"publicservicesgovt",
+ "Real Estate"=>"realestate",
+ "Religious Organizations"=>"religiousorgs",
+ "Restaurants"=>"restaurants",
+ "Shopping"=>"shopping"}
+
+ tag_names.each |k, v|
+  Tag.create(name: k, alias: v)
+end
