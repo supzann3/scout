d29161e938e2a0e288634c11931a6337078761d4
NancyKo <konancys@gmail.com>
2015-09-30 14:51:43 -0400

    Add save itinerary button

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,18 +1,22 @@
 class ItinerariesController < ApplicationController
-  def create
-    @yelp = YelpApi.new
-    @activities = []
-    search = params["search"]["ids"]
-    search.each do |k, v|
-      @yelp.set_params(v["term"],v["tag"])
-      @yelp.search
-      @activities << @yelp.results.shuffle.pop
-    end
-
-    # This should only happen if User clicks save
-    # @itinerary = Itinerary.create
-    # @itinerary.add_activities(@activities)
-
-    render 'show'
+  def new  
   end
+
+  def create 
+    if params["commit"] == "Search"
+      @yelp = YelpApi.new
+      @activities = []
+      search = params["search"]["ids"]
+      search.each do |k, v|
+        @yelp.set_params(v["term"],v["tag"])
+        @yelp.search
+        @activities << @yelp.results.shuffle.pop
+      end
+      render 'show'
+    else 
+      @itinerary = Itinerary.new
+      @itinerary.add_activities(@activities)
+    end
+  end
+  
 end
