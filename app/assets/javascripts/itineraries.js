3a6f24766592f6f0b5ff6555cd4cc2d8f4589964
supzann3 <susan.hehe01@gmail.com>
2015-10-01 13:40:59 -0400

    merge locally with sortable

--- app/assets/javascripts/itineraries.js
+++ app/assets/javascripts/itineraries.js
@@ -1,10 +1,10 @@
 $(function() {
   $( "#each_activity" ).sortable({
-     update: function(event, ui) { 
-          console.log('update: '+ui.item.index())
+     update: function(event, ui) {
+          console.log('update: '+ui.item.index());
       },
-      start: function(event, ui) { 
-          console.log('start: ' + ui.item.index())
+      start: function(event, ui) {
+          console.log('start: ' + ui.item.index());
       }
   });
   $( "#each_activity" ).disableSelection();
