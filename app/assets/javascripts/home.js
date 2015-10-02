4e4d217f14e46066a237576c8b4918b5a749122a
nikila-saravanan <nikila.saravanan@gmail.com>
2015-10-02 11:54:45 -0400

    delete previous geolocation

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,30 +1,3 @@
-// $(document).ready(function(){
-//   onLoad();
-// });
-//
-// function onLoad() {
-//   $("#activity_form").submit(function(e){
-//     e.preventDefault();
-//
-//     getLocation(function(location){
-//       $('#longitude').val(location.coords.longitude);
-//       $('#latitude').val(location.coords.latitude);
-//       $('#activity_form').unbind("submit").submit();
-//     });
-//
-//   });
-//
-//   function getLocation(callback) {
-//     if (navigator.geolocation) {
-//       navigator.geolocation.getCurrentPosition(callback);
-//     } else {
-//       $('#error-msg').html("Geolocation isn't supported in your browser.");
-//     }
-//   }
-// }
-
-
-
 $(document).ready(function() {
     var max_fields      = 10; //maximum input boxes allowed
     var wrapper         = $(".input_fields_wrap"); //Fields wrapper
