0a4a87f441f23a61d2489a5dffe1d95a308e3ba3
sherriously <lusherri@gmail.com>
2015-10-05 16:59:45 -0400

    fix flash.now causing gmaps to break

--- app/assets/javascripts/itineraries.js
+++ app/assets/javascripts/itineraries.js
@@ -10,43 +10,48 @@ $(function() {
 function initMap() {
   directionsDisplay = new google.maps.DirectionsRenderer();
   directionsService = new google.maps.DirectionsService();
+
+  function calcRoute(directionsService, directionsDisplay) {
+    var waypts = [];
+    $('.ui-sortable-handle').each(function() {
+      var self = $(this);
+      waypts.push({
+        location: new google.maps.LatLng(parseFloat(self.find('#lat').val()), parseFloat(self.find('#lng').val())),
+        stopover: true
+      });
+    });
+    var request = {
+        origin: waypts[0].location,
+        destination: waypts[waypts.length-1].location,
+        waypoints: waypts.slice(1,-1),
+        optimizeWaypoints: true,
+        travelMode: google.maps.TravelMode.WALKING
+    };
+    directionsService.route(request, function(response, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(response);
+      }
+    });
+  }
+
   var handler = Gmaps.build('Google');
   handler.buildMap({ internal: {id: 'gmap'}}, function(){
     directionsDisplay.setMap(handler.getMap());
+    handler.fitMapToBounds();
   });
     calcRoute(directionsService, directionsDisplay);
+    handler.resetBounds();
   $('#refresh-map').on("click", function() {
     calcRoute(directionsService, directionsDisplay);
-  });
-}
-
-function calcRoute(directionsService, directionsDisplay) {
-  var waypts = [];
-  $('.ui-sortable-handle').each(function() {
-    var self = $(this);
-    waypts.push({
-      location: new google.maps.LatLng(parseFloat(self.find('#lat').val()), parseFloat(self.find('#lng').val())),
-      stopover: true
-    });
-  });
-  var request = {
-      origin: waypts[0].location,
-      destination: waypts[waypts.length-1].location,
-      waypoints: waypts.slice(1,-1),
-      optimizeWaypoints: true,
-      travelMode: google.maps.TravelMode.WALKING
-  };
-  directionsService.route(request, function(response, status) {
-    if (status == google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(response);
-    }
+    handler.resetBounds();
   });
 }
 
 function replaceActivity(array,i) {
   var new_item = _.sample(array);
   $("#item"+i).html(
-    new_item.hash.name + new_item.hash.location.city + new_item.hash.rating +
+    "<a href=" + new_item.hash.url + ">" + new_item.hash.name + "</a><br>" +
+    new_item.hash.location.city + ", " + new_item.hash.rating +
     "<input type='hidden' name='activity_id' id='activity_id' value=" + new_item.hash.id + ">" +
     "<input type='hidden' name='lat' id='lat' value='" + new_item.hash.location.coordinate.latitude + "'>" +
     "<input type='hidden' name='lng' id='lng' value='" + new_item.hash.location.coordinate.longitude + "'>"
