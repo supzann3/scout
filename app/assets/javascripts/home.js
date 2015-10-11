0f720415609d1c4f5f31585b88e91101bb39f1f6
sherriously <lusherri@gmail.com>
2015-10-11 17:31:03 -0400

    add login/signup as popups and fix some search form stuff

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -10,11 +10,15 @@ $(document).ready(function() {
         if(x < max_fields){ //max input box allowed
             x++; //text box increment
             j++;
-            $(wrapper).append('<div><select name="search[ids]['+j+'][tag]" id="search_ids_'+j+'_tag"><option value="active">Active Life</option><option value="arts">Arts &amp; Entertainment</option><option value="auto">Automotive</option><option value="beautysvc">Beauty &amp; Spas</option><option value="bicycles">Bicycles</option><option value="education">Education</option><option value="eventservices">Event Planning &amp; Services</option><option value="financialservices">Financial Services</option><option value="food">Food</option><option value="health">Health &amp; Medical</option><option value="homeservices">Home Services</option><option value="hotelstravel">Hotels &amp; Travel</option><option value="localflavor">Local Flavor</option><option value="localservices">Local Services</option><option value="massmedia">Mass Media</option><option value="nightlife">Nightlife</option><option value="pets">Pets</option><option value="professional">Professional Services</option><option value="publicservicesgovt">Public Services &amp; Government</option><option value="realestate">Real Estate</option><option value="religiousorgs">Religious Organizations</option><option value="restaurants">Restaurants</option><option value="shopping">Shopping</option></select><input type="text" name="search[ids]['+j+'][term]" placeholder="Optional search term" id="search_ids_'+j+'_term"><button class="remove_field">-</button></div>'); //add input box
+            $(wrapper).append('<div><select name="search[ids]['+j+'][tag]" id="select"><option value="active">Active Life</option><option value="arts">Arts &amp; Entertainment</option><option value="auto">Automotive</option><option value="beautysvc">Beauty &amp; Spas</option><option value="bicycles">Bicycles</option><option value="education">Education</option><option value="eventservices">Event Planning &amp; Services</option><option value="financialservices">Financial Services</option><option value="food">Food</option><option value="health">Health &amp; Medical</option><option value="homeservices">Home Services</option><option value="hotelstravel">Hotels &amp; Travel</option><option value="localflavor">Local Flavor</option><option value="localservices">Local Services</option><option value="massmedia">Mass Media</option><option value="nightlife">Nightlife</option><option value="pets">Pets</option><option value="professional">Professional Services</option><option value="publicservicesgovt">Public Services &amp; Government</option><option value="realestate">Real Estate</option><option value="religiousorgs">Religious Organizations</option><option value="restaurants">Restaurants</option><option value="shopping">Shopping</option></select>&nbsp;<input type="text" name="search[ids]['+j+'][term]" placeholder="Optional search term" id="select"><button class="remove_field button button-circle button-tiny"><i class="fa fa-minus"></i></button></div>'); //add input box
         }
     });
 
     $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
         e.preventDefault(); $(this).parent('div').remove(); x--;
     });
+
+  $('#modal-login').click(function() {
+    $('#modal').modal('hide');
+  });
 });
